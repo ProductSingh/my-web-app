7b227f99088ef5ff58077632886c709aea9435ca
Mankaran <mankaran43@gmail.com>
2024-05-31 16:30:49 +1000

    Changing back To vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Mankaran!');
+  res.send('Hello, Vercel!');
 });
 
 app.listen(port, () => {
